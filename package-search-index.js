dd4b988f712d4b6d5846dcf704a0ce81102fd827
salehabdulla <salehabdullaict@gmail.com>
2026-05-24 23:30:35 +0300

    files moved

---
+++ package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"com.project.gymsys"},{"l":"com.project.gymsys.business"},{"l":"com.project.gymsys.data"},{"l":"com.project.gymsys.model"},{"l":"com.project.gymsys.ui"}];updateSearchResults();
